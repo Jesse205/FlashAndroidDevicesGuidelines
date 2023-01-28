7c77382c65a05178fa46924f46dc16bd6eae9d77
Jesse205 <2055675594@qq.com>
2023-01-29 01:36:46 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    title: '刷机指南',
+    description: '很详细的刷机指南',
+    themeConfig: {
+        nav: [
+            {
+                text: '快速开始',
+                items: [
+                    { text: '申请解锁', link: '/fast/unlock.md' },
+                    { text: '下载刷机包', link: '/fast/download.md' },
+                    { text: '刷入刷机包', link: '/fast/flash.md' },
+                ]
+            },
+            {
+                text: '基础知识',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            },
+            {
+                text: '深入指南',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,50 @@
+# 刷机指南
+
+## 介绍
+
+这个是 Jesse205 搬运整理的刷机指南。
+文档仓库部署在 Gitee 上，欢迎 Star！
+
+::: tip
+此刷机指南不仅仅提供刷机的指南，还提供各种各样玩机教程
+:::
+
+> 本教程尽量保证所有资源都有一手资源获取渠道，因此您可以放心下载。（有时为了提速可能会提供二手下载渠道）
+
+## 快速开始
+
+1. [申请解锁](fast/unlock.md)
+2. [下载刷机包](fast/download.md)
+3. [刷入刷机包](fast/flash.md)
+
+## 基础知识
+
+1. 认识安卓分区
+
+## 深入“刷机”
+
+
+## 其他教程
+
+* [Magisk 中文文档]
+
+## 其他内容
+
+这些是安卓爱好者聚集地，您可以在这些社区了解更多内容：
+
+* [酷安](https://www.coolapk.com/)
+* [XDA Forums](https://forum.xda-developers.com/)
+* [Github](https://forum.xda-developers.com/)
+
+## 参与贡献
+
+1. Fork [本仓库](https://gitee.com/Jesse205/FlashAndroidDevicesGuidelines)
+2. 新建 Feat_xxx 分支
+3. 提交代码
+4. 新建 Pull Request
+
+::: tip
+您可以直接编辑文件，Gitee会自动处理一些东西。
+:::
+
+> 由于本人刷机知识短浅，文档中出现不完善的地方很正常。希望您可以帮我改进此文档！
