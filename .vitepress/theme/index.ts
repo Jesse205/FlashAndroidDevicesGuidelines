7cdec37fcb5832e7d8b3d0d247891ef61b81ddd6
Jesse205 <2055675594@qq.com>
2023-08-15 03:58:13 +0800

    精简配置代码

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,23 +6,24 @@ import Emoji from './components/Emoji.vue'
 import ToolInfo from './components/ToolInfo.vue'
 import FileInput from './components/FileInput.vue'
 
-// import './styles/override/custom.scss'
-// import './styles/override/blur.scss'
-// import './styles/override/zoom.scss'
 import './styles/index.scss'
 
-import Layout from './Layout.vue'
+import { useZoom } from './events/zoom'
 
 export default <Theme>{
-    Layout: Layout,
+    // Layout: Layout,
     extends: DefaultTheme,
     enhanceApp(ctx) {
         DefaultTheme.enhanceApp(ctx)
         const { app } = ctx
-        
+
         app.component('Score', Score)
         app.component('Emoji', Emoji)
         app.component('ToolInfo', ToolInfo)
         app.component('FileInput', FileInput)
+    },
+    setup() {
+        // Zoom
+        useZoom()
     }
 }
